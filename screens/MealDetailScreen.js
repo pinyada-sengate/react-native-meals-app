4d2468ae3d3728b788501c14ad61e01ca86b2971
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-01-24 15:49:47 +0100

    manageing redux state and dispatch action

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,27 +1,45 @@
-import { useContext, useLayoutEffect } from "react";
+// import { useContext, useLayoutEffect } from "react";
+import { useLayoutEffect } from "react";
 import { Text, Image, View, StyleSheet, ScrollView } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
 import { MEALS } from "../data/dummy-data";
 import MealDetail from "../components/MealDetail";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import IconButton from "../components/IconButton";
-import { FavoritesContext } from "../store/context/favorites-context";
+import { addFavorite, removefavorite } from "../store/redux/favorites";
+
+// import { FavoritesContext } from "../store/context/favorites-context";
 
 function MealDetailScreen({ route, navigation }) {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  // const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+
+  const dispatch = useDispatch;
 
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+  // const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+  const mealIsFavorite = favoriteMealIds.includes(mealId);
 
   function changeFavoriteStatusHandler() {
     if (mealIsFavorite) {
-      favoriteMealsCtx.removeFavorite(mealId);
+      // favoriteMealsCtx.removeFavorite(mealId);
+      dispatch(
+        removefavorite({
+          id: mealId,
+        })
+      );
     } else {
-      favoriteMealsCtx.addFavorite(mealId);
+      // favoriteMealsCtx.addFavorite(mealId);
+      dispatch(
+        addFavorite({
+          id: mealId,
+        })
+      );
     }
   }
 
--- store/redux/store.js
+++ store/redux/store.js
@@ -4,6 +4,6 @@ import favoritesReducer from "./favorites";
 
 export const store = configureStore({
   reducer: {
-    favoriteMeal: favoritesReducer,
+    favoriteMeals: favoritesReducer,
   },
 });
